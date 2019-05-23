225371f95fa84117ba379ad5a3a45607098bad0b
alexandre-w <alexandre.git67@gmail.com>
2019-05-23 20:04:20 +0200

    setup the project with some classes

---
+++ BudgetApi/Models/Depense.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace BudgetApi.Models
+{
+    public class Depense
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Montant")]
+        public double Montant { get; set; }
+
+        [BsonElement("DateDepense")]
+        public DateTime DateDepense { get; set; }
+
+        [BsonElement("MoyenPaiement")]
+        public string MoyenPaiement { get; set; }
+
+        [BsonElement("ParQui")]
+        public string ParQui { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+
+    }
+}
---
+++ BudgetApi/Services/DepenseServices.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BudgetApi.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace BudgetApi.Services
+{
+
+    
+    public class DepenseServices
+    {
+        private readonly IMongoCollection<Depense> _depense;
+
+        public DepenseServices(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("DepenseStoreDB"));
+            var database = client.GetDatabase("DepenseStoreDB");
+            _depense = database.GetCollection<Depense>("Depenses");
+        }
+    }
+}
